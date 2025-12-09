311f8d1c7a54c580a9cf8f51a69eecabbded6656
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:58:54 +0100

    .

--- app/Filament/Forms/Components/DownloadAttachmentPlaceHolder.php
+++ app/Filament/Forms/Components/DownloadAttachmentPlaceHolder.php
@@ -1,9 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
 declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Forms\Components;
@@ -19,34 +15,10 @@ class DownloadAttachmentPlaceHolder extends Placeholder
     {
         parent::setUp();
         $this->label('')->content($this->generateContent(...))->columnSpanFull();
-<<<<<<< HEAD
-=======
-namespace Modules\Cms\Filament\Forms\Components;
-
-use Illuminate\Support\HtmlString;
-use Modules\Cms\Models\Attachment;
-use Filament\Forms\Components\Placeholder;
-
-class DownloadAttachmentPlaceHolder extends Placeholder
-{
-    
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->label('')
-         ->content(fn() => $this->generateContent())
-         ->columnSpanFull();
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
     }
 
     protected function generateContent(): HtmlString
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
         $name = $this->getName();
         $attachment = Attachment::firstWhere('slug', $name);
         Assert::isInstanceOf($attachment, Attachment::class);
@@ -66,23 +38,4 @@ class DownloadAttachmentPlaceHolder extends Placeholder
 
         return new HtmlString($out->render());
     }
-<<<<<<< HEAD
-=======
-        $name=$this->getName();
-        $attachment = Attachment::firstWhere('slug', $name);   
-        $data=[
-            'title'=>$attachment->title,
-            'description'=>$attachment->description,
-            'asset'=>$attachment->asset(),
-        ];
-        $view='pub_theme::filament.forms.components.download-attachment-place-holder';
-        $out=view($view,$data);
-        
-        return new HtmlString($out->render());
-    }
-
-    
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 }
--- resources/views/components/button/link/v1.blade.php
+++ resources/views/components/button/link/v1.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 @if (isset($attrs['onclick']))
     <button {{ $attributes->merge($attrs) }} {{-- data-bs-toggle="offcanvas" --}}>
         {{-- <i class="{{ $link->icon }}"></i> --}}
