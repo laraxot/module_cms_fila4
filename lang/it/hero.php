3b50cf401fa11e81695253903cf4b7780c52c5bd
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:53:50 +0100

    .

--- lang/it/hero.php
+++ lang/it/hero.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -54,3 +55,66 @@ return [
         ],
     ],
 ];
+=======
+return array (
+  'fields' => 
+  array (
+    'title' => 
+    array (
+      'label' => 'title',
+      'placeholder' => 'title',
+      'helper_text' => 'title',
+      'description' => 'title',
+    ),
+    'subtitle' => 
+    array (
+      'label' => 'subtitle',
+      'placeholder' => 'subtitle',
+      'helper_text' => 'subtitle',
+      'description' => 'subtitle',
+    ),
+    'image' => 
+    array (
+      'label' => 'image',
+      'placeholder' => 'image',
+      'helper_text' => 'image',
+      'description' => 'image',
+    ),
+    'cta_text' => 
+    array (
+      'label' => 'cta_text',
+      'placeholder' => 'cta_text',
+      'helper_text' => 'cta_text',
+      'description' => 'cta_text',
+    ),
+    'cta_link' => 
+    array (
+      'label' => 'cta_link',
+      'placeholder' => 'cta_link',
+      'helper_text' => 'cta_link',
+      'description' => 'cta_link',
+    ),
+    'background_color' => 
+    array (
+      'label' => 'background_color',
+      'placeholder' => 'background_color',
+      'helper_text' => 'background_color',
+      'description' => 'background_color',
+    ),
+    'text_color' => 
+    array (
+      'label' => 'text_color',
+      'placeholder' => 'text_color',
+      'helper_text' => 'text_color',
+      'description' => 'text_color',
+    ),
+    'cta_color' => 
+    array (
+      'label' => 'cta_color',
+      'placeholder' => 'cta_color',
+      'helper_text' => 'cta_color',
+      'description' => 'cta_color',
+    ),
+  ),
+);
+>>>>>>> 3401a6b (.)
