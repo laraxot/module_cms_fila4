0bc30d0656c3e8e57365d96d7f235348f1fc395d
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:53:59 +0100

    .

--- tests/Pest.php
+++ tests/Pest.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 use Tests\TestCase;
 
 /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
  * |--------------------------------------------------------------------------
  * | Test Case
  * |--------------------------------------------------------------------------
@@ -17,27 +13,10 @@ use Tests\TestCase;
  * | Utilizza il TestCase globale di Laravel con setup specifico per frontend.
  * |
  */
-<<<<<<< HEAD
-=======
-|--------------------------------------------------------------------------
-| Test Case
-|--------------------------------------------------------------------------
-|
-| Il TestCase di default per tutti i test del modulo Cms.
-| Utilizza il TestCase globale di Laravel con setup specifico per frontend.
-|
-*/
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 
 uses(Modules\Cms\Tests\TestCase::class)->in('Feature', 'Unit');
 
 /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
  * |--------------------------------------------------------------------------
  * | Expectations
  * |--------------------------------------------------------------------------
@@ -47,30 +26,12 @@ uses(Modules\Cms\Tests\TestCase::class)->in('Feature', 'Unit');
  * | in tutti i test del modulo.
  * |
  */
-<<<<<<< HEAD
-=======
-|--------------------------------------------------------------------------
-| Expectations
-|--------------------------------------------------------------------------
-|
-| Qui puoi definire aspettative globali per il modulo Cms.
-| Quando definisci here expectation globali, saranno disponibili 
-| in tutti i test del modulo.
-|
-*/
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 
 // expect()->extend('toBeValidHtml', function () {
 //     return $this->toContain('<html');
 // });
 
 /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
  * |--------------------------------------------------------------------------
  * | Functions
  * |--------------------------------------------------------------------------
@@ -83,20 +44,3 @@ uses(Modules\Cms\Tests\TestCase::class)->in('Feature', 'Unit');
 // function createTestUser() {
 //     return User::factory()->create();
 // }
-<<<<<<< HEAD
-=======
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| Qui puoi definire funzioni helper globali per i test del modulo.
-| Queste funzioni saranno disponibili in tutti i test.
-|
-*/
-
-// function createTestUser() {
-//     return User::factory()->create();
-// } 
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
