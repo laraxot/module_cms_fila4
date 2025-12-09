1f4869b8b8054a6786a07d33943f828e0c153def
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:10:48 +0100

    .

--- resources/views/admin/index/acts/sort.blade.php
+++ resources/views/admin/index/acts/sort.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 @extends('adm_theme::layouts.app')
 @section('content')
     <livewire:panel.sort />
--- resources/views/blocks/aesthetic-hero.blade.php
+++ resources/views/blocks/aesthetic-hero.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 <div class="relative bg-white/70 backdrop-blur-md rounded-lg overflow-hidden p-8 shadow-lg">
     <img src="{{ $image }}" alt="" class="absolute inset-0 w-full h-full object-cover opacity-20" />
     <div class="relative z-10 text-center">
