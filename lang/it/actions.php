405cab7ff624f21b82fc62589d4968bd519e1c6d
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:29:33 +0100

    .

--- lang/it/actions.php
+++ lang/it/actions.php
@@ -1,13 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -218,77 +210,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-return array (
-  'fields' => 
-  array (
-    'items' => 
-    array (
-      'label' => 'items',
-      'placeholder' => 'items',
-      'helper_text' => 'items',
-      'description' => 'items',
-    ),
-    'label' => 
-    array (
-      'label' => 'label',
-      'placeholder' => 'label',
-      'helper_text' => 'label',
-      'description' => 'label',
-    ),
-    'url' => 
-    array (
-      'label' => 'url',
-      'placeholder' => 'url',
-      'helper_text' => 'url',
-      'description' => 'url',
-    ),
-    'style' => 
-    array (
-      'label' => 'style',
-      'placeholder' => 'style',
-      'helper_text' => 'style',
-      'description' => 'style',
-    ),
-    'icon' => 
-    array (
-      'label' => 'icon',
-      'placeholder' => 'icon',
-      'helper_text' => 'icon',
-      'description' => 'icon',
-    ),
-    'size' => 
-    array (
-      'label' => 'size',
-      'placeholder' => 'size',
-      'helper_text' => 'size',
-      'description' => 'size',
-    ),
-    'alignment' => 
-    array (
-      'label' => 'alignment',
-      'placeholder' => 'alignment',
-      'helper_text' => 'alignment',
-      'description' => 'alignment',
-    ),
-    'gap' => 
-    array (
-      'label' => 'gap',
-      'placeholder' => 'gap',
-      'helper_text' => 'gap',
-      'description' => 'gap',
-    ),
-  ),
-);
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
--- resources/views/filament/infolists/entries/section-preview.blade.php
+++ resources/views/filament/infolists/entries/section-preview.blade.php
@@ -1,26 +1,8 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 @php
     $section = $getRecord();
     $currentLocale = app()->getLocale();
@@ -60,20 +42,4 @@ declare(strict_types=1);
             Nessuna traduzione disponibile per la lingua corrente
         </div>
     @endif
-<<<<<<< HEAD
 </div>
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-</div>
-=======
-</div> 
->>>>>>> a12f125f4a (.)
-=======
-</div>
->>>>>>> b93ef594b4 (.)
-=======
-</div> 
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
