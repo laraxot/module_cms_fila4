754fbc83538ad02ec7d0c8e8df232144384c47f8
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:12:53 +0100

    .

--- resources/views/sections/preview.blade.php
+++ resources/views/sections/preview.blade.php
@@ -1,11 +1,45 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
 @php
     //$section = $getRecord();
     //dddx($section);
 @endphp
 
+<<<<<<< HEAD
 <x-section :slug="$section->slug" />
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<x-section :slug="$section->slug" />
+=======
+<x-section :slug="$section->slug" />
+>>>>>>> a12f125f4a (.)
+=======
+<x-section :slug="$section->slug" />
+>>>>>>> b93ef594b4 (.)
+=======
+<x-section :slug="$section->slug" />
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
