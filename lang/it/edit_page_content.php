79279f2e9246a221e72e8a8dbcdf9b888dc8bd58
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:06:24 +0100

    .

--- lang/it/edit_page_content.php
+++ lang/it/edit_page_content.php
@@ -1,6 +1,9 @@
 <?php
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1377a46 (.)
 declare(strict_types=1);
 
 return [
@@ -96,6 +99,7 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
 =======
 return array (
   'fields' => 
@@ -222,3 +226,5 @@ return array (
   ),
 );
 >>>>>>> 3401a6b (.)
+=======
+>>>>>>> 1377a46 (.)
