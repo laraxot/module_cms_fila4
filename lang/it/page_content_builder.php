4bab887aa1354c751653dc7270c7ccb37790c1ef
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:51:29 +0100

    .

--- lang/it/page_content_builder.php
+++ lang/it/page_content_builder.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -12,3 +20,37 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'blocks' => 
+    array (
+      'label' => 'blocks',
+      'placeholder' => 'blocks',
+      'helper_text' => 'blocks',
+      'description' => 'blocks',
+    ),
+<<<<<<< HEAD
+    'view' => 
+    array (
+      'label' => 'view',
+      'placeholder' => 'view',
+      'helper_text' => 'view',
+      'description' => 'view',
+    ),
+  ),
+);
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+  ),
+);
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
--- tests/Feature/Frontoffice/FolioRoutes/RegistrationThankYouRouteTest.php
+++ tests/Feature/Frontoffice/FolioRoutes/RegistrationThankYouRouteTest.php
@@ -2,9 +2,19 @@
 
 declare(strict_types=1);
 
+<<<<<<< HEAD
 use Modules\Cms\Tests\TestCase;
 
 uses(TestCase::class);
+=======
+<<<<<<< HEAD
+use Modules\Cms\Tests\TestCase;
+
+uses(TestCase::class);
+=======
+uses(\Modules\Cms\Tests\TestCase::class);
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
 
 it('GET /it/registration/thank-you acceptable', function (): void {
     $res = $this->get('/it/registration/thank-you');
