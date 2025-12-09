e8d0f8cd1fbce528a299eb9cc0ddd5c21e833d7c
marco76tv <marco.sottana@gmail.com>
2025-12-09 10:55:07 +0100

    .

--- lang/it/feature_sections.php
+++ lang/it/feature_sections.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -30,3 +38,49 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'title' => 
+    array (
+      'label' => 'title',
+      'placeholder' => 'title',
+      'helper_text' => 'title',
+      'description' => 'title',
+    ),
+    'sections' => 
+    array (
+      'label' => 'sections',
+      'placeholder' => 'sections',
+      'helper_text' => 'sections',
+      'description' => 'sections',
+    ),
+    'description' => 
+    array (
+      'label' => 'description',
+      'placeholder' => 'description',
+      'helper_text' => 'description',
+      'description' => 'description',
+    ),
+    'icon' => 
+    array (
+      'label' => 'icon',
+      'placeholder' => 'icon',
+      'helper_text' => 'icon',
+      'description' => 'icon',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
