b2aea02e3f5faefb42c0265793c957d969f5d1c3
marco76tv <marco.sottana@gmail.com>
2025-12-09 16:12:14 +0100

    .

--- resources/views/components/page-content.blade.php
+++ resources/views/components/page-content.blade.php
@@ -1,8 +1,39 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 @foreach($blocks as $block)
     @include($block->view,$block->data)
 @endforeach
+=======
+<<<<<<< HEAD
+@foreach($blocks as $block)
+    @include($block->view,$block->data)
+@endforeach
+=======
+=======
+>>>>>>> origin/develop
+@foreach($blocks as $block)
+    @include($block->view,$block->data)
+@endforeach
+
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+@foreach($blocks as $block)
+    @include($block->view,$block->data)
+@endforeach
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
