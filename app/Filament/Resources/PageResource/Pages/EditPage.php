d46b3f6979c491ac49f77b739570836ec09befc4
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:59:00 +0100

    .

--- app/Filament/Resources/PageResource/Pages/EditPage.php
+++ app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -5,9 +5,46 @@ declare(strict_types=1);
 namespace Modules\Cms\Filament\Resources\PageResource\Pages;
 
 use Modules\Cms\Filament\Resources\PageResource;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> 815ce17 (.)
 use Modules\Lang\Filament\Resources\Pages\LangBaseEditRecord;
 
 class EditPage extends LangBaseEditRecord
 {
     protected static string $resource = PageResource::class;
+<<<<<<< HEAD
+=======
+=======
+use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
+=======
+>>>>>>> b93ef594b4 (.)
+use Modules\Lang\Filament\Resources\Pages\LangBaseEditRecord;
+use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
+
+class EditPage extends LangBaseEditRecord
+{
+    protected static string $resource = PageResource::class;
+<<<<<<< HEAD
+
+
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
+use Modules\Lang\Filament\Resources\Pages\LangBaseEditRecord;
+
+class EditPage extends LangBaseEditRecord
+{
+
+
+    protected static string $resource = PageResource::class;
+
+
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
 }
