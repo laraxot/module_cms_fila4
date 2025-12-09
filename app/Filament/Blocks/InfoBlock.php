cd46148134186cc9be7727cf58f50bc6f0ad30e1
marco76tv <marco.sottana@gmail.com>
2025-12-09 15:58:33 +0100

    .

--- app/Filament/Blocks/InfoBlock.php
+++ app/Filament/Blocks/InfoBlock.php
@@ -8,6 +8,7 @@ use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\TextInput;
 use Modules\Xot\Filament\Blocks\XotBaseBlock;
+use Override;
 
 final class InfoBlock extends XotBaseBlock
 {
--- database/Migrations/2024_01_01_000004_create_menus_table.php
+++ database/Migrations/2024_01_01_000004_create_menus_table.php
@@ -8,7 +8,8 @@ use Modules\Xot\Database\Migrations\XotBaseMigration;
 /*
  * Class .
  */
-return new class extends XotBaseMigration {
+return new class extends XotBaseMigration
+{
     /**
      * Run the migrations.
      */
@@ -23,11 +24,11 @@ return new class extends XotBaseMigration {
         });
         // -- UPDATE --
         $this->tableUpdate(function (Blueprint $table): void {
-            if (!$this->hasColumn('items')) {
+            if (! $this->hasColumn('items')) {
                 $table->text('items')->nullable();
             }
 
-            if (!$this->hasColumn('parent_id')) {
+            if (! $this->hasColumn('parent_id')) {
                 $table->unsignedBigInteger('parent_id')->nullable();
             }
             if ($this->hasColumn('name')) {
