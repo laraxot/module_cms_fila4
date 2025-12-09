95d517fb048cc187c5a2d8a3d6ef14eb45436131
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:28:55 +0100

    .

--- app/Config/xra.php
+++ app/Config/xra.php
@@ -1,10 +1,28 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 use Modules\User\Models\Profile;
 use Modules\User\Models\User;
 
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+declare(strict_types=1);
+
+
+=======
+>>>>>>> a12f125f4a (.)
+=======
+declare(strict_types=1);
+
+
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
 return [
     'adm_theme' => 'AdminLTE',
     'enable_ads' => false,
@@ -14,4 +32,20 @@ return [
         'profile' => Profile::class,
         'user' => User::class,
     ],
+<<<<<<< HEAD
 ];
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+];
+=======
+]; 
+>>>>>>> a12f125f4a (.)
+=======
+];
+>>>>>>> b93ef594b4 (.)
+=======
+]; 
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
--- lang/it/paragraph.php
+++ lang/it/paragraph.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -12,3 +20,28 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'content' => 
+    array (
+      'label' => 'content',
+      'placeholder' => 'content',
+      'helper_text' => 'content',
+      'description' => 'content',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
