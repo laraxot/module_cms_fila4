9de63eeea546f939f646e0c5f6c96f78ebc4c9c8
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:28:09 +0100

    .

--- app/Filament/Fields/LeftSidebarContent.php
+++ app/Filament/Fields/LeftSidebarContent.php
@@ -8,8 +8,19 @@ use Filament\Forms\Components\Builder;
 
 class LeftSidebarContent
 {
+<<<<<<< HEAD
     public static function make(string $name, string $_context = 'form'): Builder
     {
         return Builder::make($name)->blocks([])->collapsible();
+=======
+    public static function make(
+        string $name,
+        string $context = 'form',
+    ): Builder {
+        return Builder::make($name)
+            ->blocks([
+            ])
+            ->collapsible();
+>>>>>>> 3401a6b (.)
     }
 }
