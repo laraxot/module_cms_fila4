2fbc8296026bb81ed196c53c453bfdad4b2b7c55
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:27:25 +0100

    .

--- resources/views/components/page-content.blade.php
+++ resources/views/components/page-content.blade.php
@@ -1,4 +1,7 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1377a46 (.)
 <?php
 
 declare(strict_types=1);
@@ -7,9 +10,12 @@ declare(strict_types=1);
 @foreach($blocks as $block)
     @include($block->view,$block->data)
 @endforeach
+<<<<<<< HEAD
 =======
 @foreach($blocks as $block)
     @include($block->view,$block->data)
 @endforeach
 
 >>>>>>> 3401a6b (.)
+=======
+>>>>>>> 1377a46 (.)
