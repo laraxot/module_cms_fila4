ee8814754a92e16152f353b9f236ef15036a0bca
marco76tv <marco.sottana@gmail.com>
2025-12-09 23:27:33 +0100

    .

--- lang/it/stats.php
+++ lang/it/stats.php
@@ -1,13 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -38,49 +30,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-return array (
-  'fields' => 
-  array (
-    'title' => 
-    array (
-      'label' => 'title',
-      'placeholder' => 'title',
-      'helper_text' => 'title',
-      'description' => 'title',
-    ),
-    'stats' => 
-    array (
-      'label' => 'stats',
-      'placeholder' => 'stats',
-      'helper_text' => 'stats',
-      'description' => 'stats',
-    ),
-    'number' => 
-    array (
-      'label' => 'number',
-      'placeholder' => 'number',
-      'helper_text' => 'number',
-      'description' => 'number',
-    ),
-    'label' => 
-    array (
-      'label' => 'label',
-      'placeholder' => 'label',
-      'helper_text' => 'label',
-      'description' => 'label',
-    ),
-  ),
-);
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
