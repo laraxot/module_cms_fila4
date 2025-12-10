01f1a419953f453bdcf990ddb0a557ef81aa1367
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:08:13 +0100

    .

--- app/Filament/Resources/PageResource/Pages/EditPage.php
+++ app/Filament/Resources/PageResource/Pages/EditPage.php
@@ -5,28 +5,9 @@ declare(strict_types=1);
 namespace Modules\Cms\Filament\Resources\PageResource\Pages;
 
 use Modules\Cms\Filament\Resources\PageResource;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
 use Modules\Lang\Filament\Resources\Pages\LangBaseEditRecord;
 
 class EditPage extends LangBaseEditRecord
 {
     protected static string $resource = PageResource::class;
-<<<<<<< HEAD
-=======
-use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
-use Modules\Lang\Filament\Resources\Pages\LangBaseEditRecord;
-
-class EditPage extends LangBaseEditRecord
-{
-
-
-    protected static string $resource = PageResource::class;
-
-
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 }
