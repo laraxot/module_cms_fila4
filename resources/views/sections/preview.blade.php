5b7f2d6f37dc937ea3a5c986d2c26a31a1a982dc
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:34:49 +0100

    .

--- resources/views/sections/preview.blade.php
+++ resources/views/sections/preview.blade.php
@@ -1,28 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 @php
     //$section = $getRecord();
     //dddx($section);
 @endphp
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 <x-section :slug="$section->slug" />
-=======
-<x-section :slug="$section->slug" />
->>>>>>> 3401a6b (.)
-=======
-<x-section :slug="$section->slug" />
->>>>>>> 1377a46 (.)
