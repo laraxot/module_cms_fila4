4dd34f1f5166dfd825152a73aaea786d4e40dbb9
marco76tv <marco.sottana@gmail.com>
2025-12-10 09:09:56 +0100

    .

--- resources/views/livewire/menu/builder/menus.blade.php
+++ resources/views/livewire/menu/builder/menus.blade.php
@@ -1,11 +1,17 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1377a46 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 =======
 >>>>>>> 3401a6b (.)
+=======
+>>>>>>> 1377a46 (.)
 <div id="draggable" class="ui-widget-content">
     <p>Drag me to my target</p>
 </div>
