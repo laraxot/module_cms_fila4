6f3121dccfc2efd6a92cb9c7ee54e65e4567d0aa
marco76tv <marco76tv@users.noreply.github.com>
2025-12-10 08:10:32 +0000

    Check & fix styling

--- app/Filament/Blocks/HeroBlock.php
+++ app/Filament/Blocks/HeroBlock.php
@@ -8,7 +8,6 @@ use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\TextInput;
 use Modules\Xot\Filament\Blocks\XotBaseBlock;
-use Override;
 
 class HeroBlock extends XotBaseBlock
 {
--- generate_test_data.php
+++ generate_test_data.php
@@ -2,53 +2,53 @@
 
 declare(strict_types=1);
 
-use Modules\Cms\Database\Factories\ConfFactory;
-use Modules\Cms\Database\Factories\MenuFactory;
-use Modules\Cms\Database\Factories\ModuleFactory;
-use Modules\Cms\Database\Factories\PageFactory;
-use Modules\Cms\Database\Factories\PageContentFactory;
-use Modules\Cms\Database\Factories\SectionFactory;
-use Modules\Gdpr\Database\Factories\ConsentFactory;
-use Modules\Gdpr\Database\Factories\EventFactory;
-use Modules\Gdpr\Database\Factories\ProfileFactory;
-use Modules\Gdpr\Database\Factories\TreatmentFactory;
-use Modules\Lang\Database\Factories\PostFactory;
-use Modules\Lang\Database\Factories\TranslationFactory;
-use Modules\Lang\Database\Factories\TranslationFileFactory;
-use Modules\Media\Database\Factories\MediaFactory;
-use Modules\Media\Database\Factories\MediaConvertFactory;
-use Modules\Media\Database\Factories\TemporaryUploadFactory;
 use Illuminate\Contracts\Console\Kernel;
 use Modules\Cms\Database\Factories\ConfFactory;
 use Modules\Cms\Database\Factories\ConfFactory;
+use Modules\Cms\Database\Factories\ConfFactory;
+use Modules\Cms\Database\Factories\MenuFactory;
 use Modules\Cms\Database\Factories\MenuFactory;
 use Modules\Cms\Database\Factories\MenuFactory;
 use Modules\Cms\Database\Factories\ModuleFactory;
 use Modules\Cms\Database\Factories\ModuleFactory;
+use Modules\Cms\Database\Factories\ModuleFactory;
+use Modules\Cms\Database\Factories\PageContentFactory;
 use Modules\Cms\Database\Factories\PageContentFactory;
 use Modules\Cms\Database\Factories\PageContentFactory;
 use Modules\Cms\Database\Factories\PageFactory;
 use Modules\Cms\Database\Factories\PageFactory;
+use Modules\Cms\Database\Factories\PageFactory;
+use Modules\Cms\Database\Factories\SectionFactory;
 use Modules\Cms\Database\Factories\SectionFactory;
 use Modules\Cms\Database\Factories\SectionFactory;
 use Modules\Gdpr\Database\Factories\ConsentFactory;
 use Modules\Gdpr\Database\Factories\ConsentFactory;
+use Modules\Gdpr\Database\Factories\ConsentFactory;
+use Modules\Gdpr\Database\Factories\EventFactory;
 use Modules\Gdpr\Database\Factories\EventFactory;
 use Modules\Gdpr\Database\Factories\EventFactory;
 use Modules\Gdpr\Database\Factories\ProfileFactory;
 use Modules\Gdpr\Database\Factories\ProfileFactory;
+use Modules\Gdpr\Database\Factories\ProfileFactory;
+use Modules\Gdpr\Database\Factories\TreatmentFactory;
 use Modules\Gdpr\Database\Factories\TreatmentFactory;
 use Modules\Gdpr\Database\Factories\TreatmentFactory;
 use Modules\Lang\Database\Factories\PostFactory;
 use Modules\Lang\Database\Factories\PostFactory;
+use Modules\Lang\Database\Factories\PostFactory;
+use Modules\Lang\Database\Factories\TranslationFactory;
 use Modules\Lang\Database\Factories\TranslationFactory;
 use Modules\Lang\Database\Factories\TranslationFactory;
 use Modules\Lang\Database\Factories\TranslationFileFactory;
 use Modules\Lang\Database\Factories\TranslationFileFactory;
+use Modules\Lang\Database\Factories\TranslationFileFactory;
+use Modules\Media\Database\Factories\MediaConvertFactory;
 use Modules\Media\Database\Factories\MediaConvertFactory;
 use Modules\Media\Database\Factories\MediaConvertFactory;
 use Modules\Media\Database\Factories\MediaFactory;
 use Modules\Media\Database\Factories\MediaFactory;
+use Modules\Media\Database\Factories\MediaFactory;
+use Modules\Media\Database\Factories\TemporaryUploadFactory;
 use Modules\Media\Database\Factories\TemporaryUploadFactory;
 use Modules\Media\Database\Factories\TemporaryUploadFactory;
 
@@ -134,7 +134,7 @@ class TestDataGenerator
             }
 
             // Create factory instance and generate records
-            $factory = new $factoryClass;
+            $factory = new $factoryClass();
 
             // Check if the factory has the count method (Laravel Factory pattern)
             if (method_exists($factory, 'count')) {
@@ -232,7 +232,7 @@ class TestDataGenerator
 
 // Execute the generator
 try {
-    $generator = new TestDataGenerator;
+    $generator = new TestDataGenerator();
     $generator->generateTestData();
     $generator->generateTinkerCommands();
 } catch (Exception $e) {
