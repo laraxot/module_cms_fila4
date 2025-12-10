b9669bd51d7a68928960c51b84ed6209848c43f2
marco76tv <marco.sottana@gmail.com>
2025-12-10 10:35:02 +0100

    .

--- app/Filament/Resources/MenuResource/Pages/ListMenus.php
+++ app/Filament/Resources/MenuResource/Pages/ListMenus.php
@@ -4,45 +4,11 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Resources\MenuResource\Pages;
 
-<<<<<<< HEAD
 use Filament\Actions\CreateAction;
 use Filament\Tables;
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-use Filament\Actions\CreateAction;
-use Filament\Tables;
-=======
-use Filament\Tables;
-use Filament\Actions\CreateAction;
->>>>>>> a12f125f4a (.)
-=======
-use Filament\Actions\CreateAction;
-use Filament\Tables;
->>>>>>> b93ef594b4 (.)
-=======
-use Filament\Tables;
-use Filament\Actions\CreateAction;
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
-
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 class ListMenus extends XotBaseListRecords
 {
     /**
@@ -53,15 +19,7 @@ class ListMenus extends XotBaseListRecords
     public function getTableColumns(): array
     {
         return [
-<<<<<<< HEAD
             TextColumn::make('title'),
-=======
-<<<<<<< HEAD
-            TextColumn::make('title'),
-=======
-            Tables\Columns\TextColumn::make('title'),
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
         ];
     }
     // protected static string $resource = MenuResource::class;
--- tests/Feature/Frontoffice/FolioRoutes/AuthVerifyRouteTest.php
+++ tests/Feature/Frontoffice/FolioRoutes/AuthVerifyRouteTest.php
@@ -2,19 +2,9 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 use Modules\Cms\Tests\TestCase;
 
 uses(TestCase::class);
-=======
-<<<<<<< HEAD
-use Modules\Cms\Tests\TestCase;
-
-uses(TestCase::class);
-=======
-uses(\Modules\Cms\Tests\TestCase::class);
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 
 it('GET /it/auth/verify acceptable', function (): void {
     /** @phpstan-ignore-next-line property.notFound */
