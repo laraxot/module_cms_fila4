c08ac4334a5a1c6df56a477effcee8bf6ab5719c
marco76tv <marco.sottana@gmail.com>
2025-12-11 15:26:27 +0100

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -4,8 +4,16 @@ declare(strict_types=1);
 
 namespace Modules\Cms\Filament\Pages;
 
+use Filament\Facades\Filament;
+use Filament\Panel;
+use Filament\Support\Facades\FilamentIcon;
+use Filament\Widgets\Widget;
+use Filament\Widgets\WidgetConfiguration;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Facades\Route;
 use Modules\Xot\Filament\Pages\XotBaseDashboard;
 
 class Dashboard extends XotBaseDashboard
 {
+    
 }
