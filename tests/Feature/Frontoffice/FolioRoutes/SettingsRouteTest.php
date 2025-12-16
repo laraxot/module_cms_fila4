e0e5d70d036a8781e109b7206ef61afa8a1c68c4
marco76tv <marco.sottana@gmail.com>
2025-12-16 14:06:27 +0100

    .

--- tests/Feature/Frontoffice/FolioRoutes/SettingsRouteTest.php
+++ tests/Feature/Frontoffice/FolioRoutes/SettingsRouteTest.php
@@ -2,19 +2,9 @@
 
 declare(strict_types=1);
 
-<<<<<<< HEAD
 use Modules\Cms\Tests\TestCase;
 
 uses(TestCase::class);
-=======
-<<<<<<< HEAD
-use Modules\Cms\Tests\TestCase;
-
-uses(TestCase::class);
-=======
-uses(\Modules\Cms\Tests\TestCase::class);
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 
 it('GET /it/settings acceptable (likely auth required)', function (): void {
     $res = $this->get('/it/settings');
