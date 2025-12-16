afe2225e178c0c7ddd9dbe05fc5f2e75de12b71e
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:16:16 +0100

    .

--- lang/it/social.php
+++ lang/it/social.php
@@ -1,13 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -38,49 +30,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-return array (
-  'fields' => 
-  array (
-    'title' => 
-    array (
-      'label' => 'title',
-      'placeholder' => 'title',
-      'helper_text' => 'title',
-      'description' => 'title',
-    ),
-    'social_links' => 
-    array (
-      'label' => 'social_links',
-      'placeholder' => 'social_links',
-      'helper_text' => 'social_links',
-      'description' => 'social_links',
-    ),
-    'platform' => 
-    array (
-      'label' => 'platform',
-      'placeholder' => 'platform',
-      'helper_text' => 'platform',
-      'description' => 'platform',
-    ),
-    'url' => 
-    array (
-      'label' => 'url',
-      'placeholder' => 'url',
-      'helper_text' => 'url',
-      'description' => 'url',
-    ),
-  ),
-);
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,24 +1,6 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
 @include('adm_theme::admin.dashboard.' . config('xra.adm_home', '01'))
