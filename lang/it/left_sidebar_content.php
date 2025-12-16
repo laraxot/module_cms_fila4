5901dc15be408c5082a45ed74a0784c1b6905f01
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:15:13 +0100

    .

--- lang/it/left_sidebar_content.php
+++ lang/it/left_sidebar_content.php
@@ -1,3 +1,11 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
@@ -9,3 +17,26 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+<?php return array (
+  'fields' => 
+  array (
+    'sidebar_blocks' => 
+    array (
+      'label' => 'sidebar_blocks',
+    ),
+  ),
+<<<<<<< HEAD
+);
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+);
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
