5e7f8abbe31b92752aa9012b7fdfe1c32d46b8af
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:16:47 +0100

    .

--- resources/views/filament/infolists/components/section-preview.blade.php
+++ resources/views/filament/infolists/components/section-preview.blade.php
@@ -1,8 +1,26 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
 @php
     $section = $getRecord();
     $content = $section->content;
@@ -18,4 +36,20 @@ declare(strict_types=1);
             </div>
         @endif
     </div>
+<<<<<<< HEAD
 </div>
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+</div>
+=======
+</div> 
+>>>>>>> a12f125f4a (.)
+=======
+</div>
+>>>>>>> b93ef594b4 (.)
+=======
+</div> 
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
