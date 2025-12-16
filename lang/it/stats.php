448e9a84fa4936b5b42c63ca6feac989a3d7414d
marco76tv <marco.sottana@gmail.com>
2025-12-16 14:04:29 +0100

    .

--- lang/it/stats.php
+++ lang/it/stats.php
@@ -1,5 +1,13 @@
 <?php
 
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -30,3 +38,49 @@ return [
         ],
     ],
 ];
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+=======
+>>>>>>> origin/develop
+return array (
+  'fields' => 
+  array (
+    'title' => 
+    array (
+      'label' => 'title',
+      'placeholder' => 'title',
+      'helper_text' => 'title',
+      'description' => 'title',
+    ),
+    'stats' => 
+    array (
+      'label' => 'stats',
+      'placeholder' => 'stats',
+      'helper_text' => 'stats',
+      'description' => 'stats',
+    ),
+    'number' => 
+    array (
+      'label' => 'number',
+      'placeholder' => 'number',
+      'helper_text' => 'number',
+      'description' => 'number',
+    ),
+    'label' => 
+    array (
+      'label' => 'label',
+      'placeholder' => 'label',
+      'helper_text' => 'label',
+      'description' => 'label',
+    ),
+  ),
+);
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
--- resources/views/components/button/panel/edit/v1.blade.php
+++ resources/views/components/button/panel/edit/v1.blade.php
@@ -1,8 +1,39 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> b93ef594b4 (.)
+>>>>>>> 815ce17 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
+<<<<<<< HEAD
 <a {{ $attributes->merge($attrs) }}>
     {!! $icon !!}
 </a>
+=======
+<<<<<<< HEAD
+<a {{ $attributes->merge($attrs) }}>
+    {!! $icon !!}
+</a>
+=======
+=======
+>>>>>>> origin/develop
+<a {{ $attributes->merge($attrs) }}>
+    {!! $icon !!}
+</a>
+
+<<<<<<< HEAD
+>>>>>>> a12f125f4a (.)
+=======
+<a {{ $attributes->merge($attrs) }}>
+    {!! $icon !!}
+</a>
+>>>>>>> b93ef594b4 (.)
+=======
+>>>>>>> origin/develop
+>>>>>>> 815ce17 (.)
