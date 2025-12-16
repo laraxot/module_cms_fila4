10f4f91b3e0f8b8df523eaa4a3fbfb10d785116e
marco76tv <marco.sottana@gmail.com>
2025-12-16 08:24:28 +0100

    .

--- resources/views/components/button/link/tab.blade.php
+++ resources/views/components/button/link/tab.blade.php
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 <li class="nav-item">
     <a {{ $attributes->merge($attrs) }}>{{ $link->title }}</a>
 </li>
