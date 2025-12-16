3748e4dd291ca81419f996f37e1c56ff88ac4d90
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:16:06 +0100

    .

--- lang/it/quick_links.php
+++ lang/it/quick_links.php
@@ -1,5 +1,6 @@
 <?php
 
+<<<<<<< HEAD
 declare(strict_types=1);
 
 return [
@@ -36,3 +37,45 @@ return [
         ],
     ],
 ];
+=======
+return array (
+  'fields' => 
+  array (
+    'title' => 
+    array (
+      'label' => 'title',
+      'placeholder' => 'title',
+      'helper_text' => 'title',
+      'description' => 'title',
+    ),
+    'links' => 
+    array (
+      'label' => 'links',
+      'placeholder' => 'links',
+      'helper_text' => 'links',
+      'description' => 'links',
+    ),
+    'label' => 
+    array (
+      'label' => 'label',
+      'placeholder' => 'label',
+      'helper_text' => 'label',
+      'description' => 'label',
+    ),
+    'url' => 
+    array (
+      'label' => 'url',
+      'placeholder' => 'url',
+      'helper_text' => 'url',
+      'description' => 'url',
+    ),
+    'target' => 
+    array (
+      'label' => 'target',
+      'placeholder' => 'target',
+      'helper_text' => 'target',
+      'description' => 'target',
+    ),
+  ),
+);
+>>>>>>> 3401a6b (.)
