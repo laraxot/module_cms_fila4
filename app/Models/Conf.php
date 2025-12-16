165ab4b8114ce712177d0677c5ea57a57d00d9f0
marco76tv <marco.sottana@gmail.com>
2025-12-16 19:16:44 +0100

    .

--- app/Models/Conf.php
+++ app/Models/Conf.php
@@ -48,17 +48,8 @@ class Conf extends BaseModel
 
     /** @var list<string> */
     protected $fillable = [
-<<<<<<< HEAD
-<<<<<<< HEAD
         'id',
         'name',
-=======
-        'id', 'name',
->>>>>>> 3401a6b (.)
-=======
-        'id',
-        'name',
->>>>>>> 1377a46 (.)
     ];
 
     /**
@@ -73,23 +64,10 @@ class Conf extends BaseModel
     }
 
     /*
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
      * protected function sushiShouldCache() {
      * return false;
      * }
      */
-<<<<<<< HEAD
-=======
-    protected function sushiShouldCache() {
-        return false;
-    }
-    */
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
     /**
      * Undocumented function.
      */
--- resources/views/components/button/action/v2.blade.php
+++ resources/views/components/button/action/v2.blade.php
@@ -1,29 +1,12 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 1377a46 (.)
 <?php
 
 declare(strict_types=1);
 
 ?>
-<<<<<<< HEAD
-=======
->>>>>>> 3401a6b (.)
-=======
->>>>>>> 1377a46 (.)
 <button {{ $attributes->merge($attrs) }} {{-- data-bs-toggle="offcanvas" --}}>
         {{-- <i class="{{ $link->icon }}"></i> --}}
         {!! $icon !!}
 </button>
 {{--
 {!! $action->btnHtml() !!}
-<<<<<<< HEAD
-<<<<<<< HEAD
 --}}
-=======
---}}
->>>>>>> 3401a6b (.)
-=======
---}}
->>>>>>> 1377a46 (.)
