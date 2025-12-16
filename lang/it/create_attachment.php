753f84afb77c5c99094a5bd37046ed6a0ded0e3a
marco76tv <marco.sottana@gmail.com>
2025-12-16 14:03:37 +0100

    .

--- lang/it/create_attachment.php
+++ lang/it/create_attachment.php
@@ -1,13 +1,5 @@
 <?php
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b93ef594b4 (.)
->>>>>>> 815ce17 (.)
 declare(strict_types=1);
 
 return [
@@ -26,37 +18,3 @@ return [
         ],
     ],
 ];
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-=======
->>>>>>> origin/develop
-return array (
-  'actions' => 
-  array (
-    'activeLocale' => 
-    array (
-      'label' => 'activeLocale',
-    ),
-    'create' => 
-    array (
-      'label' => 'create',
-    ),
-    'createAnother' => 
-    array (
-      'label' => 'createAnother',
-    ),
-    'cancel' => 
-    array (
-      'label' => 'cancel',
-    ),
-  ),
-);
-<<<<<<< HEAD
->>>>>>> a12f125f4a (.)
-=======
->>>>>>> b93ef594b4 (.)
-=======
->>>>>>> origin/develop
->>>>>>> 815ce17 (.)
