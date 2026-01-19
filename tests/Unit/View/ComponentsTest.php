d2b09e3df96b8b72818426aff2c641672d2dbe73
marco76tv <marco76tv@users.noreply.github.com>
2026-01-19 20:17:26 +0000

    Check & fix styling

--- tests/Unit/View/ComponentsTest.php
+++ tests/Unit/View/ComponentsTest.php
@@ -2,26 +2,26 @@
 
 declare(strict_types=1);
 
-uses(\Modules\Cms\Tests\TestCase::class);
+uses(Modules\Cms\Tests\TestCase::class);
 
-use Modules\Cms\View\Components\Section;
 use Modules\Cms\View\Components\Page;
 use Modules\Cms\View\Components\PageContent;
+use Modules\Cms\View\Components\Section;
 
 test('Section component can be instantiated', function () {
     $component = new Section();
-    
+
     expect($component)->toBeInstanceOf(Section::class);
 });
 
 test('Page component can be instantiated', function () {
     $component = new Page();
-    
+
     expect($component)->toBeInstanceOf(Page::class);
 });
 
 test('PageContent component can be instantiated', function () {
     $component = new PageContent();
-    
+
     expect($component)->toBeInstanceOf(PageContent::class);
 });
